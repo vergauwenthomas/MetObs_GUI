29f29a151b1a7eb5d3565a9bcdb8188b446bed62
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-08-10 15:43:02 +0200

    dep testing

--- tests/push_test/gui_launch_test.py
+++ tests/push_test/gui_launch_test.py
@@ -5,14 +5,9 @@ Created on Sun Apr 16 20:20:47 2023
 
 @author: thoverga
 """
-import sys
 import metobs_gui
 
-is_succesfull = metobs_gui.launch_gui()
-if is_succesfull:
-    sys.exit() #normal exit
-else:
-    sys.exit('Problem launching GUI!')
+metobs_gui.launch_gui()
 
 
 
